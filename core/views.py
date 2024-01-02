ad7828a5bb7109fbc58ef73109985bf5c1f965da
ashwabu <abdulazizredi90@gmail.com>
2024-01-02 18:17:04 +0300

    booking view

--- core/views.py
+++ core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from formtools.wizard.views import SessionWizardView
-from .forms import GuestDetail,BusinessDetails
+from .forms import GuestDetail,BusinessDetails,BookingForm
 
 # Create your views here.
 class BookWizardView(SessionWizardView):
-    form_list = [GuestDetail,BusinessDetails]
+    form_list = [GuestDetail,BusinessDetails,BookingForm]
     template_name = 'core/index.html'
     def done(self, form_list, **kwargs):
         return HttpResponse('form submitted')
