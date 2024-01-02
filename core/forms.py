27fc3ded307e7ee9453b1394e00f70599a3a59b1
ashwabu <abdulazizredi90@gmail.com>
2024-01-02 17:16:57 +0300

    first step

--- core/forms.py
+++ core/forms.py
@@ -1,1 +1,11 @@
 from django import forms
+from .models import Guest
+
+class GuestDetail(forms.ModelForm):
+    Bool_options = [{True: 'Yes', False: 'No'}]
+    is_business = forms.BooleanField(
+        widget= forms.RadioSelect(choices=Bool_options, default=False,required=False)
+    )
+    class Meta:
+        model = Guest
+        fields = ('first_name', 'last_name', 'email','phone' )
